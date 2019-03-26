f2284c84afd33ee58f5fc6daa49519dcc2d6c8a9
norimasanoguchi <norimasanoguchi@gmail.com>
2019-03-26 00:05:59 +0000

    Step19,20ユーザー登録、ログイン画面実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum:30 }
+  validates :email, presence: true, length: { maximum:255 }
+  validates :password, presence: true, length: { minimum: 6 }
+  before_validation { email.downcase! }
+  has_secure_password
 end
---
+++ db/migrate/20190325225734_add_password_to_user.rb
@@ -0,0 +1,6 @@
+class AddPasswordToUser < ActiveRecord::Migration[5.2]
+  def change
+    add_column(:users, :password_digest, :string)
+    add_index(:users, :email, unique: true)
+  end
+end
