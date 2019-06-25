113f758237dd43b5f48f4edcb33f8066bb6e33f2
Noguchi Norimasa <norimasanoguchi@gmail.com>
2019-06-25 22:32:36 +0000

    Fix:管理者ユーザー削除制限メソッド名変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum:255 }
   validates :password, presence: true, length: { minimum: 6 }
   before_validation { email.downcase! }
-  before_destroy :block_destroy
+  before_destroy :block_delete_last_admin
   has_secure_password
 
   private
-  def block_destroy
+  def block_delete_last_admin
     raise if User.where(admin: true).count == 1
   end
 end
